0a4bdb21c351643d2f54c5b60648ee584c1be0ca
Mayasa164 <139007766+Mayasa164@users.noreply.github.com>
2023-08-30 14:32:50 +0400

    try_type_function

---
+++ try_type_function/bin/try_type_function.dart
@@ -0,0 +1,33 @@
+void main(){
+
+}
+
+//1)no return , no parameter
+
+void printHi(){
+print("Hi");
+}
+//2) return , no parameter
+
+String getName(){
+  return" Amna";
+}
+
+
+//3)no return ,  parameter
+void setName (String name){
+  print(name);
+}
+
+
+
+//4) return ,  parameter
+
+int calculate(int x , int y){
+return x + y ;
+}
+
+
+
+
+
---
+++ try_type_function/test/try_type_function_test.dart
@@ -0,0 +1,8 @@
+import 'package:try_type_function/try_type_function.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
